97192ad3d0aa6202ed5e08b9c70881a9cd203065
Caresa Huddleston <caresa.huddleston@gmail.com>
2014-06-04 10:10:39 -0500

    first commit

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,16 @@
 class Pizza
+
+  # def initialize(topping)
+
+  # end
 end
 
 class Topping
+attr_accessor :name, :vegetarian
+
+  def initialize(name, vegetarian: false)
+    @name = name
+    @vegetarian = vegetarian
+  end
+
 end
